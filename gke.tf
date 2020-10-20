6af9222b5b06c6bc784533832801e6e7af207c4a
jcote5150 <joelcote68@gmail.com>
2020-10-20 10:49:38 -0500

    updated network for deployments

--- gke.tf
+++ gke.tf
@@ -9,6 +9,7 @@ resource "google_container_cluster" "engineering" {
   # node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
+  subnetwork = "infra-hashi-subnet-001"
 
   master_auth {
     username = var.username
